ad405f4e978a06f9905745f5234ba40c5c977a39
HannahSantos <hannah.santos.441@ufrn.edu.br>
2024-10-24 22:25:18 -0300

    Alguns teoremas adicionados

---
+++ FMCnLean/myNat/Theorems.lean
@@ -0,0 +1,25 @@
+import FMCnLean.myNat.Definitions
+
+open myNat
+
+theorem add_zero :
+  ∀ (n : myNat), add n O = n :=
+by
+  sorry
+  -- Escreva aqui sua demonstração
+  -- Identação é importante :)
+
+theorem max_zero :
+  ∀ (n : myNat), max n O = n :=
+by
+  sorry
+
+theorem mul_one :
+  ∀ (n : myNat), mul n (S O) = n :=
+by
+  sorry
+
+theorem exp_one :
+  ∀ (n : myNat), exp n (S O) = n :=
+by
+  sorry
