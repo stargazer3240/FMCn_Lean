cea0e505cb60aea6ae56ab43741c6990a4b9c7e1
HannahSantos <hannah.santos.441@ufrn.edu.br>
2024-10-24 22:14:01 -0300

    Added new files

--- FMCnLean.lean
+++ FMCnLean.lean
@@ -1,4 +1,6 @@
 -- This module serves as the root of the `FMCnLean` library.
 -- Import modules here that should be built as part of the library.
 import «FMCnLean».Basic
-import «FMCnLean».myNat
+import «FMCnLean».myNat.Definitions
+import «FMCnLean».myNat.Theorems
+import «FMCnLean».myBool.Definitions
